aa6ad7282d48eb2bd9f1ac2fdfe61263932630a0
Krupananda Reddy <krupa8522@gmail.com>
2023-07-09 21:27:17 +0530

    Initial Test commits

--- main.tf
+++ main.tf
@@ -3,7 +3,13 @@ resource "local_file" "pet" {
   content="I am looking to get a pet for my daughter"
   file_permission = "0700"
 }
+resource "random_pet" "test_random" {
 
+}
 output "pet_output" {
   value = local_file.pet.filename
+}
+
+output "random_pet_output" {
+  value = random_pet.test_random.id
 }
